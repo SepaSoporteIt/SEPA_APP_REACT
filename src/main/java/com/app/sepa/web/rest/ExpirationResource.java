a2370c28a9aac8775a1a5e5373d56387630eadc4
Juan Pablo Maggi <juanpablomaggiv@gmail.com>
2020-07-18 18:02:50 -0300

    Added GenerateUniqueCode, GenerateResponsible and CheckDate functions to the expirations

--- src/main/java/com/app/sepa/web/rest/ExpirationResource.java
+++ src/main/java/com/app/sepa/web/rest/ExpirationResource.java
@@ -1,6 +1,7 @@
 package com.app.sepa.web.rest;
 
 import com.app.sepa.domain.Expiration;
+import com.app.sepa.domain.enumeration.Status;
 import com.app.sepa.repository.ExpirationRepository;
 import com.app.sepa.web.rest.errors.BadRequestAlertException;
 
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -35,6 +37,8 @@ public class ExpirationResource {
     private final Logger log = LoggerFactory.getLogger(ExpirationResource.class);
 
     private static final String ENTITY_NAME = "expiration";
+
+    private List<Expiration> expirationList;
 
     @Value("${jhipster.clientApp.name}")
     private String applicationName;
@@ -59,6 +63,9 @@ public class ExpirationResource {
             throw new BadRequestAlertException("A new expiration cannot already have an ID", ENTITY_NAME, "idexists");
         }
         Expiration result = expirationRepository.save(expiration);
+        GenerateUniqueCode(result);
+        GenerateResponsible(result);
+        CheckDatesLogic();
         return ResponseEntity.created(new URI("/api/expirations/" + result.getId()))
             .headers(HeaderUtil.createEntityCreationAlert(applicationName, true, ENTITY_NAME, result.getId().toString()))
             .body(result);
@@ -80,6 +87,9 @@ public class ExpirationResource {
             throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
         }
         Expiration result = expirationRepository.save(expiration);
+        GenerateUniqueCode(result);
+        GenerateResponsible(result);
+        CheckDatesLogic();
         return ResponseEntity.ok()
             .headers(HeaderUtil.createEntityUpdateAlert(applicationName, true, ENTITY_NAME, expiration.getId().toString()))
             .body(result);
@@ -92,7 +102,15 @@ public class ExpirationResource {
      * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of expirations in body.
      */
     @GetMapping("/expirations")
-    public ResponseEntity<List<Expiration>> getAllExpirations(Pageable pageable) {
+    public ResponseEntity<List<Expiration>> getAllExpirations(Pageable pageable) 
+    {
+        expirationList = expirationRepository.findAll();
+        for (Expiration expiration : expirationList)
+        {
+            GenerateUniqueCode(expiration);
+            GenerateResponsible(expiration);
+        }
+        CheckDatesLogic();
         log.debug("REST request to get a page of Expirations");
         Page<Expiration> page = expirationRepository.findAll(pageable);
         HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(ServletUriComponentsBuilder.fromCurrentRequest(), page);
@@ -106,7 +124,9 @@ public class ExpirationResource {
      * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the expiration, or with status {@code 404 (Not Found)}.
      */
     @GetMapping("/expirations/{id}")
-    public ResponseEntity<Expiration> getExpiration(@PathVariable Long id) {
+    public ResponseEntity<Expiration> getExpiration(@PathVariable Long id) 
+    {
+        CheckDatesLogic();
         log.debug("REST request to get Expiration : {}", id);
         Optional<Expiration> expiration = expirationRepository.findById(id);
         return ResponseUtil.wrapOrNotFound(expiration);
@@ -123,5 +143,95 @@ public class ExpirationResource {
         log.debug("REST request to delete Expiration : {}", id);
         expirationRepository.deleteById(id);
         return ResponseEntity.noContent().headers(HeaderUtil.createEntityDeletionAlert(applicationName, true, ENTITY_NAME, id.toString())).build();
+    }
+
+    /**
+     * Generates the expiration's responsible according to the company employee.
+     * @param actualExpiration the expiration to set the responsible.
+     */
+    public void GenerateResponsible(Expiration actualExpiration)
+    {
+        String responsible;
+
+        if (actualExpiration.getCompany() == null)
+        {
+            actualExpiration.setResponsible(null);
+        }
+        else
+        {
+            if (actualExpiration.getCompany().getEmployee()==null)
+            {
+                actualExpiration.setResponsible(null);
+            }
+            else
+            {
+                responsible = actualExpiration.getCompany().getEmployee().getName() + " " + actualExpiration.getCompany().getEmployee().getSurname();
+                actualExpiration.setResponsible(responsible);
+            }
+        }  
+    }
+
+    /**
+     * Generates the unique code using the company, employee, study and start date of the expiration
+     * @param actualExpiration The expiration to modify
+     */
+    public void GenerateUniqueCode(Expiration actualExpiration)
+    {
+        String companyId;
+        String employeeId;
+        String studyId;
+
+        if (actualExpiration.getCompany() == null)
+        {
+            companyId = "0";
+        }
+        else
+        {
+            companyId = actualExpiration.getCompany().getId().toString();
+        }
+
+        if (actualExpiration.getEmployee() == null)
+        {
+            employeeId = "0";
+        }
+        else
+        {
+            employeeId = actualExpiration.getEmployee().getId().toString();
+        }
+
+        if (actualExpiration.getStudy() == null)
+        {
+            studyId = "0";
+        }
+        else
+        {
+            studyId = actualExpiration.getStudy().getId().toString();
+        }
+        
+        String initialDate = actualExpiration.getStartDate().toString();
+        actualExpiration.setUniqueCode(companyId+"-"+employeeId+"-"+studyId+"-"+initialDate);
+    }
+
+    /**
+     * Change the state of all expirations based on the actual date and the due date.
+     */
+    public void CheckDatesLogic()
+    {
+        expirationList = expirationRepository.findAll();
+
+        for (Expiration expiration : expirationList) {
+
+            LocalDate actualEndDate = expiration.getEndDate();
+            LocalDate actualWarningDate = actualEndDate.minusDays(30);
+
+            if (actualEndDate.isBefore(LocalDate.now()))
+            {
+                expiration.setStatus(Status.VENCIDO);
+            }
+            else if (actualWarningDate.isBefore(LocalDate.now()))
+            {
+                expiration.setStatus(Status.A_VENCER);
+            }
+        }
     }
 }
