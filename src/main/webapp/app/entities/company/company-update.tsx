cff9ff6a8c5472c5aaf7da01611b1a6e9cc2cedf
Juan Pablo Maggi <juanpablomaggiv@gmail.com>
2021-01-16 11:05:03 -0300

    CIUU helper and name in selection

--- src/main/webapp/app/entities/company/company-update.tsx
+++ src/main/webapp/app/entities/company/company-update.tsx
@@ -255,6 +255,11 @@ export const CompanyUpdate = (props: ICompanyUpdateProps) => {
                 <Translate contentKey="sepaApp.company.detail.afipActivity">Activity stablished by AFIP</Translate>
               </h3>
               <AvGroup>
+                <Label>
+                  <Translate contentKey="sepaApp.company.helpers.ciiuHelper">CIUU helper</Translate>
+                </Label>
+              </AvGroup>
+              <AvGroup>
                 <Label for="company-industryType">
                   <Translate contentKey="sepaApp.company.industryType">1º CIIU</Translate>
                 </Label>
@@ -263,7 +268,7 @@ export const CompanyUpdate = (props: ICompanyUpdateProps) => {
                   {industryTypes
                     ? industryTypes.map(otherEntity => (
                         <option value={otherEntity.id} key={otherEntity.id}>
-                          {otherEntity.ciiu}
+                          {otherEntity.ciiu + " - " + otherEntity.name}
                         </option>
                       ))
                     : null}
@@ -278,7 +283,7 @@ export const CompanyUpdate = (props: ICompanyUpdateProps) => {
                   {industryTypes
                     ? industryTypes.map(otherEntity => (
                         <option value={otherEntity.id} key={otherEntity.id}>
-                          {otherEntity.ciiu}
+                          {otherEntity.ciiu + " - " + otherEntity.name}
                         </option>
                       ))
                     : null}
